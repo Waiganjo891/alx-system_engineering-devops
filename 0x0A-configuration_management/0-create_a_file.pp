0f750b27e7de5c67c2c9b2155884edd2a2df5c57
Your Name <you@example.com>
2024-03-24 10:15:58 -0700

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# creates a file in /tmp
-
 file { '/tmp/school':
-ensure	=> file,
-content	=> 'I Love Puppet',
-mode	=> '07744',
-owner	=> 'www-data',
-group	=> 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
 }
