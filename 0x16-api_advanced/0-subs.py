abe6898114f3b61948bdc274fd87e044e5b3d33d
Your Name <you@example.com>
2024-05-07 04:13:58 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,9 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscriber
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
